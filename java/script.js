114dd5ad7d03273bacd4e8d29d2bdc036ea1a388
Haji Bamsi <hibamsi@gmail.com>
2022-03-06 13:25:09 +0300

    add playground function

--- java/script.js
+++ java/script.js
@@ -1,7 +1,20 @@
+/**
+ *
+ * @returns the random item from the list
+ */
 function computerPlay() {
   let itemList = ["Rock", "Paper", "Scissor"];
   let index = Math.floor(Math.random() * itemList.length);
   return itemList[index];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    let result = `"You win ${playerSelection} beats ${computerSelection}`;
+    return result;
+  } else {
+    let result = `"You Lose! ${playerSelection} beats ${computerSelection}`;
+    return result;
+  }
+}
+console.log(playRound("rock", computerPlay()));
