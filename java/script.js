e9009a48ea5d6f7a43907a35f2db03fed6345c91
Haji Bamsi <hibamsi@gmail.com>
2022-03-08 17:43:01 +0300

    create game function

--- java/script.js
+++ java/script.js
@@ -10,11 +10,32 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-    let result = `"You win ${playerSelection} beats ${computerSelection}`;
-    return result;
+    let result = `${playerSelection} is incorrect guess, Try again! `;
+    alert(result);
+    return false;
   } else {
-    let result = `"You Lose! ${playerSelection} beats ${computerSelection}`;
-    return result;
+    let result = `${playerSelection} is correct guess, Play again! `;
+    alert(result);
+    return true;
   }
 }
-console.log(playRound("rock", computerPlay()));
+
+function game() {
+  let win = 0;
+  for (let i = 0; i < 5; i++) {
+    let value = window.prompt("Enter rock, paper or scissor");
+    let result = playRound(value, computerPlay());
+    if (result) {
+      win++;
+    }
+  }
+  //decide a winner when he/ she scores more than 2 attempt
+  if (win > 2) {
+    alert("Congratulations! You won");
+  } else {
+    alert("Sorry! You lose");
+  }
+}
+
+//call game function
+game();
